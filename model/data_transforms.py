ddfde6ecc7ff56f5b6b31d78aa5c1468aeb939e3
ljazzal <jazzalin.main@gmail.com>
2020-12-17 23:13:03 -0500

    modified rescrop to work for test set

--- model/data_transforms.py
+++ model/data_transforms.py
@@ -58,6 +58,53 @@ class JointRescrop(object):
         return img, target
 
 
+class JointRescropTest(object):
+    """
+    Rescrop transform when we only know the 2D box (e.g. test set)
+    """
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, img, target):
+        target_q = target[:4]
+        target_r = target[4:7]
+        target_db = target[7:11] 
+        target_bb = target[11:]
+
+        sz = self.size[0]
+        xmin, ymin, xmax, ymax = target_db
+        x = [xmin, xmax]
+        y = [ymin, ymax]
+        fS = 1.75
+        dx = xmax - xmin
+        dy = ymax - ymin
+        self.cropSize = int(fS * max(dx, dy))
+        if self.cropSize > min(img.size):
+            self.cropSize = min(img.size)
+        
+        # Crop Image
+        self.cx = sum(x)/len(x)
+        self.cy = sum(y)/len(y)      
+        top = self.cy - self.cropSize/2
+        left = self.cx - self.cropSize/2
+        img = tF.crop(img, top, left, self.cropSize, self.cropSize)
+        
+        # Crop translation of points
+        xtarget = [self.cropSize/2 + (i - self.cx) for i in x]
+        ytarget = [self.cropSize/2 + (i - self.cy) for i in y]
+        
+        # Rescale to input size
+        norm = img.size[1]
+        img = tF.resize(img, sz)
+
+        # Update db from bb
+        target_db = [sz*xtarget[0]/norm, sz*ytarget[0]/norm, sz*xtarget[1]/norm, sz*ytarget[1]/norm]
+        
+        target = np.concatenate((target_q, target_r, target_db, target_bb))
+
+        return img, target
+
+
 class JointToTensor(object):
     def __call__(self, img, target):
         return tF.to_tensor(img), torch.from_numpy(target)
